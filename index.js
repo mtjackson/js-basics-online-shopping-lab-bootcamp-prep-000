422913f039b5f646652fd2bf476b51b42405114e
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-13 04:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,4 +70,5 @@ function placeOrder(cardNumber) {
     return "Your total cost is $" + totalPrice + ", which will be charged to the card " + cardNumber + ".";
   } if (cardNumber.length === 0) {
   return "Sorry, we don't have a credit card on file for you.";
+  }
 }
