578b164a9ea04c5fe20e98d683353726a49f3b9e
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-13 04:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (typeof cardNumber === undefined) {
+  if (cardNumber === undefined) {
   return "Sorry, we don't have a credit card on file for you.";
   } else if (typeof cardNumber !== undefined) {
     var totalPrice = 0
